704710281ab99d5f1e2f33d1fbb74a70035ed056
Szymon <szymon@localhost.localdomain>
2020-11-25 18:20:30 +0100

    clocks

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <random>
+#include <stdlib.h>
+#include <time.h>
 
 class Boom {
 public:
@@ -15,6 +16,7 @@ public:
         explosion.setPosition(0.0f, 0.0f);
         explosion.setTexture(&txt);
         explosion.setTextureRect(sf::IntRect(dif, 0, 96, 96));
+        explosion.setOrigin(sf::Vector2f(48.0f, 48.0f));
     }
 
     void update(sf::RenderTarget &target) {
@@ -75,18 +77,79 @@ public:
     void restart() {
         isBoomed = false;
     }
+
+    void setpos(sf::Vector2f pos) {
+        explosion.setPosition(pos);
+    }
+};
+
+class Rocket {
+public:
+    bool isLose;
+    int score;
+    bool ifNotGenerated;
+    sf::Texture texture;
+    sf::RectangleShape rect;
+    Boom boom;
+
+    Rocket() {
+        isLose = false;
+        score = 0;
+        ifNotGenerated = false;
+        texture.loadFromFile("res/bomb.png");
+
+        rect.setSize(sf::Vector2f(texture.getSize().x * 5, texture.getSize().y * 5));
+        rect.setTexture(&texture);
+        rect.setOrigin(sf::Vector2f(rect.getSize().x / 2, rect.getSize().y / 2));
+        rect.setPosition(-100.0f, -100.0f);
+        rect.setRotation(90); // 70
+    }
+
+    void update(sf::RenderTarget &target, sf::Vector2f mousePos) {
+        if(!ifNotGenerated) {
+            isLose = false;
+            float x = (rand() % 600) + 100.0f;
+            rect.setPosition(x, -50.0f);
+            ifNotGenerated = true;
+        }
+
+        rect.move(sf::Vector2f(0.0f, 5.0f));
+
+        target.draw(rect);
+
+        if(rect.getPosition().y > 550.0f) {
+            boom.setpos(rect.getPosition());
+            boom.update(target);
+            if(!boom.isBoomed) {
+                isLose = true;
+                score = 0;
+                boom.restart();
+                ifNotGenerated = false;
+            }
+        }
+
+        if(rect.getGlobalBounds().contains(mousePos) && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            score++;
+            ifNotGenerated = false;
+        }
+    }
 };
 
 int main(int argv, char** argc) {
+    srand(time(NULL));
     sf::RenderWindow window(sf::VideoMode(800, 600), "Bomber", sf::Style::Default);
     window.setFramerateLimit(60);
     window.setMouseCursorVisible(false);
 
-    sf::Texture textures[7];
+    #ifdef __WIN32__
+        sf::Image icon;
+        icon.loadFromFile("res/bomb.png");
+        window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    #endif
+
+    sf::Texture textures[2];
     textures[0].loadFromFile("res/cursor.png");
     textures[1].loadFromFile("res/background.png");
-    textures[2].loadFromFile("res/bomb.png");
-    textures[3].loadFromFile("res/explosion.png");
 
     sf::RectangleShape cursor;
     cursor.setSize(sf::Vector2f(textures[0].getSize().x * 5, textures[0].getSize().y * 5));
@@ -99,7 +162,24 @@ int main(int argv, char** argc) {
     background.setPosition(0.0f, 0.0f);
     background.setTexture(&textures[1]);
 
-    Boom boom;
+    Rocket rocket;
+
+    sf::Font font;
+    font.loadFromFile("res/arial.ttf");
+
+    sf::Text text;
+    text.setPosition(0.0f, 0.0f);
+    text.setCharacterSize(20);
+    text.setFont(font);
+    text.setString("Wynik: " + std::to_string(rocket.score));
+
+    sf::Text loseText;
+    loseText.setPosition(100.0f, 100.0f);
+    loseText.setCharacterSize(40);
+    loseText.setFont(font);
+    loseText.setString("Przegrales!");
+
+    sf::Clock loseClock;
 
     bool focus = true;
     while(window.isOpen()) {
@@ -119,10 +199,23 @@ int main(int argv, char** argc) {
         window.clear(sf::Color::Green);
         window.draw(background);
 
-        boom.update(window);
+        rocket.update(window, mouse);
 
         cursor.setPosition(mouse);
         window.draw(cursor);
+
+        if(!rocket.isLose) {
+            text.setString("Wynik: " + std::to_string(rocket.score));
+            window.draw(text);
+        } else {
+            float loseTime = loseClock.getElapsedTime().asMilliseconds();
+            loseClock.restart();
+            std::cout << loseTime << "\n";
+            window.draw(loseText);
+            
+        }
+        
+        
 
         window.display();
     }
